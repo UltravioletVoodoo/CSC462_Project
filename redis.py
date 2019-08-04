1f263690abda9fef19d2ee5dd56108ebd75381ee
Joss Vrooman <vroomanj357@gmail.com>
2019-08-04 11:49:58 -0700

    added links to websites for references.

--- redis.py
+++ redis.py
@@ -1,5 +1,9 @@
+#https://github.com/andymccurdy/redis-py
+#https://github.com/bakwc/PySyncObj
+
 import redis
+import pysyncobj 
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-r.set('foo', 'bar')
-r.get('foo')
+print(r.set('foo', 'bar'))
+print(r.get('foo'))
